b862a39ad22d80cbd04a95504bfd5c1afec4e619
tonny0831 <tonnyjansen0831@gmail.com>
2022-11-18 10:25:05 +0530

    add pagination

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,22 @@
-import { gql, useApolloClient } from '@apollo/client';
-import Head from 'next/head';
-import React, { useEffect, useState } from 'react'
+import { gql, useApolloClient } from "@apollo/client";
+import Head from "next/head";
+import React, { useCallback, useEffect, useState } from "react";
 
 const mainURL = `https://arweave.net/`;
 
 const FETCH_IMAGES = gql`
-  query images($orderBy: String!, $orderDirection: String!) {
-    images(orderBy: $orderBy, orderDirection: $orderDirection) {
+  query images(
+    $orderBy: String!
+    $orderDirection: String!
+    $first: Int!
+    $skip: Int!
+  ) {
+    images(
+      orderBy: $orderBy
+      orderDirection: $orderDirection
+      first: $first
+      skip: $skip
+    ) {
       id
       image
       tags
@@ -15,33 +25,36 @@ const FETCH_IMAGES = gql`
 `;
 
 const Dashboard = () => {
+  const [page, setPage] = useState(0);
 
   const [images, setImages] = useState([]);
 
   const clientApollo = useApolloClient();
-  
-   const getImages = async () => {
-     clientApollo
-       .query({
-         query: FETCH_IMAGES,
-         variables: {
-           orderBy: "createdAt",
-           orderDirection: "desc",
-         },
-         fetchPolicy: "network-only",
-       })
-       .then(({ data }) => {
-         console.log(data)
-         setImages(data);
-       })
-       .catch((error) => {
-         console.error(error);
-       });
-  };
-  
-    useEffect(() => {
-      getImages();
-    }, [images]);
+
+  const getImages = useCallback(async () => {
+    clientApollo
+      .query({
+        query: FETCH_IMAGES,
+        variables: {
+          orderBy: "createdAt",
+          orderDirection: "desc",
+          first: 20,
+          skip: page * 20,
+        },
+        fetchPolicy: "network-only",
+      })
+      .then(({ data }) => {
+        console.log(data);
+        setImages(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },[clientApollo,page]);
+
+  useEffect(() => {
+    getImages();
+  }, [getImages,page]);
 
   return (
     <div className="font-body  relative">
@@ -66,8 +79,8 @@ const Dashboard = () => {
           </div>
         )}
 
-        <section className="max-w-[1440px] flex items-center justify-center my-0 mx-auto">
-          <div className="gap-3 columns-3 md:columns-2 sm:columns-1 md:p-2">
+        <section className="max-w-[1440px] flex flex-col items-center justify-center my-0 mx-auto">
+          <div className="gap-3 columns-3 md:columns-2 sm:columns-1 md:p-2 ">
             {images?.images?.length &&
               images?.images?.map((data) => (
                 <div key={data.id} className="relative">
@@ -77,16 +90,42 @@ const Dashboard = () => {
                     className="w-full mb-3 rounded-sm"
                   />
 
-                  <div className="absolute opacity-0 backdrop-blur-sm bg-black/50 hover:opacity-100 w-full h-full left-0 top-0 p-6  cursor-pointer transition duration-350 ease-out hover:ease-in">
+                  <div className="absolute opacity-0 backdrop-blur-sm bg-black/50 hover:opacity-100 w-full h-full left-0 top-0 p-6  cursor-pointer transition duration-350 ease-out hover:ease-in rounded-sm">
                     <h2 className="font-bold text-3xl my-2">See Details</h2>
                   </div>
                 </div>
               ))}
           </div>
+
+          {images?.images?.length < 20 ? (
+            <div>
+              <h2 className="my-2 text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-800 font-semibold">
+                You&apos;ve reached end of the list{" "}
+              </h2>
+            </div>
+          ) : (
+            <div className="flex gap-8">
+              <button
+                disabled={page === 0}
+                onClick={() => setPage((prev) => prev - 1)}
+                className="px-[25px] py-[15px] bg-[#1E50FF] outline-none border-none  rounded-3xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-115 hover:drop-shadow-xl hover:shadow-sky-600 my-[15px] w-fit"
+              >
+                Prev
+              </button>
+
+              <button
+                disabled={images?.images?.length < 20}
+                onClick={() => setPage((prev) => prev + 1)}
+                className="px-[25px] py-[15px] bg-[#1E50FF] outline-none border-none  rounded-3xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-115 hover:drop-shadow-xl hover:shadow-sky-600 my-[15px] w-fit"
+              >
+                Next
+              </button>
+            </div>
+          )}
         </section>
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
